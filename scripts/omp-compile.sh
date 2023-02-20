69678d5952cc731454d5d4fcceffde77a12a35fe
Hns Hlr <74055973+Hnshlr@users.noreply.github.com>
2023-02-20 10:26:07 +0000

    OpenMP Compile options
    Fixed the compiling options for OpenMP

--- scripts/omp-compile.sh
+++ scripts/omp-compile.sh
@@ -1,1 +1,3 @@
-cd /scratch/s388885/sspp/sspp-assignment/ && module load fosscuda/2019b && export CXX=$(which g++) && g++ -fopenmp -O4 main.cpp src/mmio.h src/mmio.c
+cd /scratch/s388885/sspp/sspp-assignment/ && module load fosscuda/2019b && export CXX=$(which g++) && g++ -fopenmp -O4 -lgomp -std=c++11 main.cpp src/mmio.h src/mmio.c
+
+# g++ -fopenmp -O4 -lgomp -std=c++11 main.cpp src/mmio.h src/mmio.c
