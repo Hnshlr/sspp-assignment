ced6400e1ce8aac84fd55b854a2938e22546e28d
Hns Hlr <74055973+Hnshlr@users.noreply.github.com>
2023-02-20 10:27:48 +0000

    OpenMP: Added more chunks values for the chunks method

--- main.cpp
+++ main.cpp
@@ -299,7 +299,7 @@ int main(int argc, char *argv[]) {
 
         // METHOD 2: VARIOUS CHUNK SIZES:
         std::vector<int> chunk_sizes = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
-                                        65536};
+                                        65536, 131072, 262144, 524288, 1048576};
         int bestChunkSize = 0;
         double bestTime = 1000000;
         for (int chunk_size: chunk_sizes) {
@@ -787,7 +787,8 @@ int main(int argc, char *argv[]) {
                std::get<1>(maxAndRelDiff));
 
         // METHOD 2: CHUNKS:
-        std::vector<int> chunk_sizes = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
+        std::vector<int> chunk_sizes = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+                                        65536, 131072, 262144, 524288, 1048576};
         int bestChunkSize = 0;
         double bestTime = 1000000;
         for (int chunk_size: chunk_sizes) {
